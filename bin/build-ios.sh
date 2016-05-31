5d84c1180818360d58c72d05173f8425df1bd9f4
Luc Michalski <michalski.luc@gmail.com>
2016-05-31 14:03:19 +0200

    Add more platforms templates, add simple build for ios bash script, add README templates to explain the structuration, description of hunterz apps

---
+++ bin/build-ios.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+EXTRA_ARGS=""
+if [ $# -ge 1 ]; then
+	EXTRA_ARGS="--clear"
+fi
+
+TOOLCHAIN=ios-9-3-arm64
+
+rename_tab gatherer $TOOLCHAIN
+
+function build_all
+{
+    COMMAND=(
+        "--toolchain ${TOOLCHAIN}"
+        "--verbose "
+        "--fwd HUNTER_CONFIGURATION_TYPES=Release "
+        "CMAKE_XCODE_ATTRIBUTE_IPHONEOS_DEPLOYMENT_TARGET=8.0 "
+        "--config Release "
+        "--jobs 8 "
+        "--open "
+        "--reconfig "
+        "--nobuild "
+	"${EXTRA_ARGS} "
+    )
+    
+	build.py  ${COMMAND[*]}
+}
+
+(cd ${DIR}/.. && build_all)
